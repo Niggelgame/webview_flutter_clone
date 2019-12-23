a044e48b7cf2992008a7d9fde876efab43176f10
Niggelgame <50844055+Niggelgame@users.noreply.github.com>
2019-12-23 20:55:19 +0100

    Resolved Typing issue

--- android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -49,7 +49,7 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
     // Allow local storage.
     webView.getSettings().setDomStorageEnabled(true);
     webView.getSettings().setLoadWithOverviewMode(true);
-    webView.getSetting().setUseWideViewPort(true);
+    webView.getSettings().setUseWideViewPort(true);
     
 
     methodChannel = new MethodChannel(messenger, "plugins.flutter.io/webview_" + id);
